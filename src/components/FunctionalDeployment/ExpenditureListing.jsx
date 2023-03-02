ba690d90189c19d5f88dc426d94714b112f51507
Rajan Chaudhari <rajanchaudharigits@gmail.com>
2023-03-02 14:32:11 +0530

    Deploying React Hook: useState

--- src/components/FunctionalDeployment/ExpenditureListing.jsx
+++ src/components/FunctionalDeployment/ExpenditureListing.jsx
@@ -1,22 +1,32 @@
 import "../FunctionalDeployment/ExpenditureListing.css";
 import ExpenditureDate from "../FunctionalDeployment/ExpenditureDate";
+import CardContainer from "../UserInterface/CardContainer";
+import { useState } from "react";
 const ExpenditureListing = (properties) => {
   // const expenseDate = new Date(2023, 3, 1);
   // const expenseTitle = "Car Insurance";
   // const expensePrice = "$294.67";
+  // console.log("Nodes are evaluated by React")
+  // let title = properties.title;
+  const [title, setTitle] = useState(properties.title);
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
   return (
-    <div className="expense-item">
+    <CardContainer className="expense-item">
       {/* <div>{expenseDate.toISOString()}</div> */}
       {/* <div>{properties.date.toISOString()}</div> */}
       {/* <div>{properties.date.toLocaleString("en-US", { month: "long" })}</div> */}
       <ExpenditureDate date={properties.date} />
       <div className="expense-item__description">
         {/* <h2>{expenseTitle}</h2> */}
-        <h2>{properties.title}</h2>
+        <h2>{title}</h2>
         {/* <div className="expense-item__price">{expensePrice}</div> */}
         <div className="expense-item__price">{properties.price}</div>
       </div>
-    </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </CardContainer>
   );
 };
 export default ExpenditureListing;
