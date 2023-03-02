5a57be8eafe1839f751d04dc72ff056648a559e5
Rajan Chaudhari <rajanchaudharigits@gmail.com>
2023-03-02 12:31:37 +0530

    Passing a string containing the class names as property. Destructuring the property in the child component.

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,7 +1,7 @@
 import "./CardContainer.css";
 
 function CardContainer(properties) {
-  const class__CardContainer = "card" + properties.className;
+  const class__CardContainer = `card ${properties.className}`;
   return <div className={class__CardContainer}>{properties.children}</div>;
 }
 
